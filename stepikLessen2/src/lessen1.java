83e9c1fe096ceef3c70df818b2237733e38d365a
Vladislav <104034207+Laslou99@users.noreply.github.com>
2023-01-25 02:49:51 +0300

    StepikLessen2

---
+++ stepikLessen2/src/lessen1.java
@@ -0,0 +1,10 @@
+public class lessen1 {
+    public static boolean booleanExpression(boolean a, boolean b, boolean c, boolean d){
+        int i = 0;
+        if (a) i++;
+        if (b) i++;
+        if (c) i++;
+        if (d) i++;
+        return i == 2;
+    }
+}
