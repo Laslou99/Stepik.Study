0d4c8bba14668c48120d3352fb716869287f3723
Vladislav <104034207+Laslou99@users.noreply.github.com>
2023-01-30 01:22:38 +0300

    Java

--- stepikLessen3/src/lessen1.java
+++ stepikLessen3/src/lessen1.java
@@ -1,3 +1,8 @@
 public class lessen1 {
-
+    public static boolean isPalindrome(String text) {
+        String newString = text.replaceAll("[^a-zA-Z0-9]","");
+        StringBuilder sb_newString = new StringBuilder(newString);
+        boolean fin = newString.equalsIgnoreCase(sb_newString.reverse().toString());
+        return fin;
+    }
 }
