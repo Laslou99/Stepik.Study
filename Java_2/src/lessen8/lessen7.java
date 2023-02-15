60e0e717b3337fb367ccc44f611c0fd854a96abe
Vladislav <104034207+Laslou99@users.noreply.github.com>
2023-02-15 19:26:52 +0300

    Java new Lessen

--- Java_2/src/lessen8/lessen7.java
+++ Java_2/src/lessen8/lessen7.java
@@ -1,4 +1,18 @@
 package lessen8;
 
+import java.util.Scanner;
+
 public class lessen7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int sum = num2;
+        int i = 1;
+        while (sum < num1) {
+            sum = num2 * i;
+            i++;
+        }
+        System.out.println(sum);
+    }
 }
--- Java_2/src/lessen8/lessen9.java
+++ Java_2/src/lessen8/lessen9.java
@@ -1,4 +1,15 @@
 package lessen8;
 
+import java.util.Scanner;
+
 public class lessen9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        String star = "*";
+        for (int i = 1; i <= a ; i++) {
+            System.out.println(star);
+            star += "*";
+        }
+    }
 }
