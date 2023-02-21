9a71029613e46720077bbe16bec82bbfebdcc1e8
Vladislav <104034207+Laslou99@users.noreply.github.com>
2023-02-21 23:11:52 +0300

    з

--- Java_2/src/lessen11/lessen3.java
+++ Java_2/src/lessen11/lessen3.java
@@ -1,4 +1,26 @@
 package lessen11;
 
+import java.util.Scanner;
+
 public class lessen3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int counter = 1;
+        int pin_code_coincided = 5;
+        while (pin_code_coincided > 0){
+            String correct = sc.nextLine();
+            String vasya = sc.nextLine();
+            if(correct.equals(vasya)){
+                System.out.println("CORRECT");
+                break;
+            }else{
+                System.out.println("INCORRECT " + counter);
+            }
+            counter++;
+            pin_code_coincided --;
+        }
+        if (pin_code_coincided == 0){
+            System.out.println("Error");
+        }
+    }
 }
--- Java_2/src/lessen11/lessen4.java
+++ Java_2/src/lessen11/lessen4.java
@@ -1,4 +1,24 @@
 package lessen11;
 
+import javax.security.sasl.SaslClient;
+import java.util.Scanner;
+
 public class lessen4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int i, year = 2020;
+        boolean is = false;
+        while(year<2300) {
+            i = k - (2 * n);
+            n++;
+            k++;
+            if (i == 0) {
+                is = true;
+                break;
+            }
+            year++;
+        }
+    }
 }
