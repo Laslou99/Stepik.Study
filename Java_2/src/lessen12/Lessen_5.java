2019b7e3990a3009a114c6b9ad7e5e8508062166
Vladislav <104034207+Laslou99@users.noreply.github.com>
2023-02-28 23:42:17 +0300

    java

--- Java_2/src/lessen12/Lessen_5.java
+++ Java_2/src/lessen12/Lessen_5.java
@@ -1,4 +1,12 @@
 package lessen12;
 
+import java.util.Scanner;
+
 public class Lessen_5 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+
+    }
 }
